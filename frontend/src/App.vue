a3164cffec16276f7195d76ea77fa7709b082e37
Md. Nazmus Saqib Khan <ratulkhan.jhenidah@gmail.com>
2021-08-29 00:43:35 +0600

    Icons added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <p class="font-body">Hi, I'm {{ name }}</p>
-    <img src="@/assets/images/banner.jpg">
+    <Navigation />
   </div>
 </template>
 
 <script>
+import Navigation from "@/components/Navigation";
+
 export default {
+  name: "FrontendApplicaion",
+  components: {
+    Navigation,
+  },
   data() {
     return {
-      name: "Nazmus Saqib"
-    }
+      name: "Nazmus Saqib",
+    };
   },
 };
 </script>
---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MenuIcon class="h-8 w-8 text-2xl font-black" />
+  </div>
+</template>
+
+<script>
+import { MenuIcon } from "@heroicons/vue/solid";
+
+export default {
+  name: "Navigation",
+  components: { MenuIcon },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
